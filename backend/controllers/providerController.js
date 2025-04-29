0839a1fba25270df44e2450a80d60fb34faa58bd
sushma <sushma.vangala3005@gmail.com>
2025-04-29 21:01:11 +0530

    bcrypt to bcryptjs

--- backend/controllers/providerController.js
+++ backend/controllers/providerController.js
@@ -1,5 +1,5 @@
 import providerModel from "../models/providerModel.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import appointmentModel from "../models/appointmentModel.js";
 
